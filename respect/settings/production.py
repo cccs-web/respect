f73939c8bba2cb31be312688ed8ca1fa3ba9ebb3
cccs-ip <cccs.dennis@gmail.com>
2014-07-26 22:34:23 -0500

    changing title and tagline

--- respect/settings/production.py
+++ respect/settings/production.py
@@ -11,8 +11,8 @@ DATABASES = {
         "HOST": "",
         "PORT": ""}}
 
-SITE_TITLE = 'The 100% Respect Campaign'
-SITE_TAGLINE = None
+SITE_TITLE = 'The 100% RESPECT! Campaign'
+SITE_TAGLINE = '100% RESPECT! for 100% of the People, 100% of the Time'
 
 ###################
 # DEPLOY SETTINGS #
