dc5bd118e933608c7ed5f3756a43369cf6fb44d5
pwhipp <paul.whipp@gmail.com>
2014-07-31 14:34:41 +1000

    try without the hyphen

--- respect/settings/ci.py
+++ respect/settings/ci.py
@@ -2,7 +2,7 @@ from .includes.common import *
 
 DEBUG = True
 
-ALLOWED_HOSTS.append(u'respect-ci.crossculturalconsult.com')
+ALLOWED_HOSTS.append(u'respectci.crossculturalconsult.com')
 
 DATABASES = {
     "default": {
